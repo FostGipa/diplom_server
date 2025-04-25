0f820f06707f2856ce157a6df0b4189491cf2cb3
FostGipa <fostgipayt@gmail.com>
2025-04-25 18:27:06 +0300

    rating fix

--- db/migrate.js
+++ db/migrate.js
@@ -23,7 +23,7 @@ const createTables = async () => {
         dobro_id int,
         help_hours int default 0,
         completed_tasks int default 0,
-        rating real default 0.0
+        rating varchar(50) default 0.0
     );
 
     CREATE TABLE IF NOT EXISTS Clients (
